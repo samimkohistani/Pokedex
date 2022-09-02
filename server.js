b90c12018c81cff114067d34d86bf865765142a3
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-01 21:04:25 -0400

    second commit

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+cnost pokemon = require("./models/pokemon")
 
-app.get("/greeting", (request, response) => {
-    response.send("Hello, stranger")
+app.get("/pokemon", (req, res) => {
+  res.render("index_views.ejs", {
+      pokemons: pokemon,
   });
+});
 
   
 
 
 
-  
+
+
+
+
+
+
+
+
 
   app.listen(port, () => {
     console.log("listening on port", port)
