28ce195f99f15c47a487120df09a14654c702fa6
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-01 23:23:30 -0400

    commiting

--- server.js
+++ server.js
@@ -2,36 +2,42 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const methodOverride = require("method-override");
 const pokemon = require("./models/pokemon");
 
 
+app.use(express.urlencoded({extended : false}));
+app.use(methodOverride("_method"));
 
-app.get("/pokemon", (req, res)=>{
-  res.render("pokemon_index.ejs", {
-      pokemons : pokemon,
-  });
+
+app.get("/pokemon", (req, res) => {
+    res.render("pokemon_index.ejs", {
+        pokemons: pokemon,
+    });
 });
 
 app.get("/pokemon/new", (req, res) => {
-  res.render("pokemon_new.ejs")
+    res.render("pokemon_new.ejs")
 });
 
-
-
-
-
-
-
-
+app.delete("/pokemon/:id", (req, res) => {
+    pokemon.splice(req.params.id, 1)
+    res.redirect("/pokemon");
+})
+app.post("/pokemon", (req, res) => {
+    pokemon.push(req.body);
+    res.redirect("/pokemon");
+});
 
 app.get("/pokemon/:id", (req, res) => {
-  res.render("pokemon_show.ejs", {
-      pokemon: pokemon[req.params.id],
-  });
+    res.render("pokemon_show.ejs", {
+        pokemon: pokemon[req.params.id],
+    });
 });
 
 
 
-  app.listen(port, () => {
-    console.log("listening on port", port)
+
+app.listen(port, () => {
+  console.log("listening on port", port)
   });
