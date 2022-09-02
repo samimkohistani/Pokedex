9e99699c56468950ff41b68795eacd760c56be4f
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-02 00:37:05 -0400

    edit/update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// DEPENDENCIES
 
 const express = require("express");
 const app = express();
@@ -9,6 +10,13 @@ const pokemon = require("./models/pokemon");
 app.use(express.urlencoded({extended : false}));
 app.use(methodOverride("_method"));
 
+// Begginer page
+
+app.get("/" , function (req, res){
+  res.send(`Wlcome to pokemon world! lets get to know our pokemons!! visit => <a href="/pokemon"> /pokemon. </a>`)
+})
+
+// Index
 
 app.get("/pokemon", (req, res) => {
     res.render("pokemon_index.ejs", {
@@ -16,18 +24,27 @@ app.get("/pokemon", (req, res) => {
     });
 });
 
+// Add new
+
 app.get("/pokemon/new", (req, res) => {
     res.render("pokemon_new.ejs")
 });
 
+// Delete
+
 app.delete("/pokemon/:id", (req, res) => {
     pokemon.splice(req.params.id, 1)
     res.redirect("/pokemon");
-})
+});
+
+// create
+
 app.post("/pokemon", (req, res) => {
     pokemon.push(req.body);
     res.redirect("/pokemon");
 });
+
+// Show
 
 app.get("/pokemon/:id", (req, res) => {
     res.render("pokemon_show.ejs", {
@@ -36,8 +53,29 @@ app.get("/pokemon/:id", (req, res) => {
 });
 
 
+// update
 
+app.put("/pokemon/:id", function(req, res){
+  pokedex[req.params.id] = req.body
+  res.redirect("/pokemon")
+});
+
+// Edit
+
+app.get("/pokemon/:id/edit", (req, res) =>{
+  res.render("pokemon_edit.ejs",
+  {
+      pokemon: pokemon[req.params.id],
+      index: req.params.id,
+  });
+});
+
+
+// App listener 
 
 app.listen(port, () => {
   console.log("listening on port", port)
   });
+
+
+//End
